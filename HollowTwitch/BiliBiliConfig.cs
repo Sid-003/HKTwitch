8a5d47adb4151e4e0a75162da0f1289eb715e97b
a2659802 <a2655536@163.com>
2020-08-14 14:25:23 +0800

    my stupid, forget to save

--- HollowTwitch/BiliBiliConfig.cs
+++ HollowTwitch/BiliBiliConfig.cs
@@ -5,7 +5,10 @@ using System.Text;
 
 namespace HollowTwitch
 {
-    class BiliBiliConfig
+    class BiliBiliConfig : TwitchConfig
     {
+        private int _room_id;
+        public new string Token { get => "Not Need Token"; }
+        public new string Channel { get => _room_id.ToString(); set => _room_id = Convert.ToInt32(value); }
     }
 }
